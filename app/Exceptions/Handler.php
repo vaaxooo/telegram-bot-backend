aca224f10edad725b9d86a006c92edf43e5c75d7
vaaxooo <donjkee47@gmail.com>
2023-01-14 12:06:20 +0100

    - fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,7 +46,64 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            return response()->json([
+                'status' => false,
+                'error' => $e->getMessage()
+            ], 500);
         });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'code' => 404,
+                    'message' => 'Method Not Found',
+                    'status' => 'error'
+                ]);
+            }
+        });
+    }
+
+
+    /**
+     * If the user is not authenticated, return a JSON response with a 401 status code
+     * 
+     * @param request The incoming request.
+     * @param AuthenticationException exception The exception that was thrown.
+     * 
+     * @return A JSON response with a 401 status code.
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json([
+            'code' => 401,
+            'message' => 'Unauthenticated',
+            'status' => 'error'
+        ]);
+    }
+
+    /**
+     * If the request is an AJAX request, return a JSON response with the error code, message, and
+     * status
+     * 
+     * @param request The current request.
+     * @param Throwable e The exception that was thrown
+     * 
+     * @return A JSON response with a 500 status code.
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($e->getMessage() == '') {
+            return response()->json([
+                'code' => 404,
+                'message' => 'Method Not Found',
+                'status' => 'error'
+            ]);
+        }
+
+        return response()->json([
+            'code' => 500,
+            'message' => $e->getMessage() || 'Internal Server Error',
+            'status' => 'error'
+        ]);
     }
 }
